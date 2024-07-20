6744a1ef9291a593ef2a72e95734d33f7565c6bc
JotaDevMan <jpea195@gmail.com>
2024-07-20 19:55:23 -0400

    avance evaluacion4

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './style.css'
+import NavBar from './Nevbar';
+import PostIt from './Post-it';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <div className='container mx-1 my-3'>
+        <NavBar></NavBar>
+    
+        <div className="container">
+            <div className="row">
+                <PostIt></PostIt>
+            </div>
+        </div>
+    </div>
+    
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
